c2e88b1f3cc9775ce219c287793f2f5e5abe85f8
Dirsu <DirsuJr@gmail.com>
2020-01-23 21:39:22 -0300

    Aula 23/01

---
+++ Exercicios/exercicio10.js
@@ -0,0 +1,14 @@
+let numero1 = prompt("Informe um número")
+let numero2 = prompt("Informe outro número")
+let T = prompt("Insira o tipo de conta, sendo elas: Ad Adição, Su Subtração, Mul Multiplicação e Div Divisão")
+
+
+numero1= parseFloat(numero1);
+numero2= parseFloat(numero2);
+
+let Ad = numero1 + numero2;
+let Su = numero1 - numero2;
+let Mul = numero1 * numero2; 
+let Div = numero1 / numero2;
+
+console.log(  )
---
+++ Exercicios/exercicio3.js
@@ -0,0 +1,8 @@
+let Numero = prompt("Insira o número");
+
+Numero= parseFloat(Numero);
+
+let Antecessor = Numero - 1;
+let Sucessor = Numero + 1;
+
+console.log("O antecessor do número " + Numero + " é " + Antecessor + " , e o sucessor é " + Sucessor + ".");
